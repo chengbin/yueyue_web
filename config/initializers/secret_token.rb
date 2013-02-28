e5355e3b4257b7692ff3347391931adb08b64b56
Ashin <sz.chengbin@gmail.com>
2013-02-28 22:12:42 +0800

    create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YueyueWeb::Application.config.secret_token = 'eb035562bf17d460ac496d20f9dedd587a5e81da2b3a2349fe477a23616fed872f039d887f2ae7da3db818c2064fc20cb5106db0be446306e147c1bc5a04db31'
