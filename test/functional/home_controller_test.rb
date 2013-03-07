1cf9ac8434d228c1ebccb5c33b877d2d5db9b53d
Ashin <sz.chengbin@gmail.com>
2013-03-07 23:42:19 +0800

    修改登录测试用例

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -7,8 +7,17 @@ class HomeControllerTest < ActionController::TestCase
   end
 
   test 'should login' do
+    assert_equal session[:user_id], nil
     chengbin1 = users(:one)
     post :login, :name => chengbin1.name, :password => 'pass'
     assert_redirected_to home_index_path
+    assert_equal session[:user_id], chengbin1.id
+  end
+  
+  test 'should login failed' do
+    assert_equal session[:user_id], nil
+    chengbin1 = users(:one)
+    post :login, :name => chengbin1.name, :password => 'no_pass'
+    assert_equal session[:user_id], nil
   end
 end
